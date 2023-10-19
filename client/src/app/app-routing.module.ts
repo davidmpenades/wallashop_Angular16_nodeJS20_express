3e97b19b0932fb63590aa25f6a03fc54215e49d8
Davidmpenades <davidmpenades@gmail.com>
2023-10-19 20:58:41 +0200

    no-guard-Auth verify

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -26,6 +26,10 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: '**',
+    redirectTo: ""
   }
 ];
 
