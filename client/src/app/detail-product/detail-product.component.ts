69ba948ba76a1475a04fb77519fcaf95ebfe0683
kevpoesp <kevposesp@gmail.com>
2023-10-31 13:32:34 +0100

    more releated in product detail

--- client/src/app/detail-product/detail-product.component.ts
+++ client/src/app/detail-product/detail-product.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Product } from '../core/model/product.model';
 import { ProductService } from '../core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
+import { Filters } from '../core/model/filters.model';
 
 @Component({
   selector: 'app-detail-product',
@@ -12,6 +14,18 @@ export class DetailProductComponent {
   product: Product = {} as Product;
   images!: String[];
   owner?: String;
+  productsReleated: Product[] = [];
+
+  filters: Filters = {
+    limit: 6,
+    offset: 0,
+    text: '',
+    price_max: 0,
+    price_min: 0,
+    category: '',
+    owner: '',
+    profileLikes: ''
+  };
 
   constructor(
     private productService: ProductService,
@@ -35,8 +49,34 @@ export class DetailProductComponent {
       next: (data) => {
         this.product = data;
         this.images = this.product.imgs
+        this.getProducts()
       },
       error: (err) => console.error(err),
     });
+  }
+
+  async getProducts() {
+
+    const params = await this.getRequestParams();
+
+    this.productService.get(params).subscribe({
+      next: (data) => {
+        this.productsReleated = data.products.filter((prod)=> prod.slug != this.product.slug)
+      },
+    });
+  }
+
+  async getRequestParams() {
+    const params = new HttpParams()
+      .set('limit', this.filters.limit.toString())
+      .set('offset', this.filters.offset.toString())
+      .set('text', this.filters.text)
+      .set('price_max', this.filters.price_max.toString())
+      .set('price_min', this.filters.price_min.toString())
+      .set('category', this.product.category)
+      .set('owner', this.filters.owner)
+      .set('profileLikes', this.filters.profileLikes)
+
+    return params;
   }
 }
